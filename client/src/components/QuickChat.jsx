8b1f29a6ef3a3a5a62f9835260def3b609826b3b
Ivan Bailey <ivangbailey@gmail.com>
2021-07-07 09:24:51 -0700

    Initial commit

---
+++ client/src/components/QuickChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuickChat = (props) => {
+  return (
+    <>
+    <h1>Quick Chat ->></h1>
+    </>
+  );
+};
+
+export default QuickChat;
---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import QuickChat from './components/QuickChat.jsx';
+
+ReactDOM.render(<QuickChat />, document.getElementById('quickchat'));
